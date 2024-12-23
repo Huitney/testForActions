52ebb588e898cbc2823ce58f2b6d4471de337f5b
huitney <wbang05@gmail.com>
2024-12-23 18:52:21 +0800

    create docker image 43

--- Dockerfile.php
+++ Dockerfile.php
@@ -17,10 +17,10 @@ RUN apt-get update && apt-get install -y \
 RUN docker-php-ext-install dom
 
 # 安裝 Composer
-RUN curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer
+RUN curl -sS https://getcomposer.org/installer | php && php composer.phar install && mv composer.phar /usr/local/bin/composer
 
 # 安裝專案依賴項
-RUN composer install
+#RUN composer install
 
 # 設置正確的權限（如果需要）
 RUN chown -R www-data:www-data /app && chmod -R 755 /app
