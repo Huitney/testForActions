bfd306f2b237d222fd876ec2ed9fa55b329a8e46
huitney <wbang05@gmail.com>
2025-01-01 19:30:30 +0800

    create docker image 14

--- Dockerfile.js
+++ Dockerfile.js
@@ -1,15 +1,13 @@
 FROM node:lts
 
-# 設置工作目錄
 WORKDIR /app
 
-# 複製代碼到容器
 COPY . /app
 
-# 安裝專案依賴項
-RUN npm install --save-dev jest jest-environment-jsdom @testing-library/dom
+RUN npm install
+
+# 安裝 Babel 和 Jest 必需的依賴
+RUN npm install --save-dev @babel/core @babel/preset-env babel-jest jest
 
 # 設置正確的權限（如果需要）
 RUN chown -R node:node /app && chmod -R 755 /app
-
-ENV NODE_ENV=test
